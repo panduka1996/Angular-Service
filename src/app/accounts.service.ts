094f115ec3986b196d1c733bf8b350ef6944e417
panduka wijekoon <42469227+panduka1996@users.noreply.github.com>
2021-01-27 09:38:53 +0530

    Add files via upload

---
+++ src/app/accounts.service.ts
@@ -0,0 +1,38 @@
+import { EventEmitter, Injectable } from '@angular/core';
+import { LoggingService } from './logging.service';
+
+@Injectable()
+export class AccountsService {
+
+    accounts = [
+        {
+            name: 'Master Account',
+            status: 'active'
+        },
+        {
+            name: 'Testaccount',
+            status: 'inactive'
+        },
+        {
+            name: 'Hidden Account',
+            status: 'unknown'
+        }
+    ];
+
+    constructor(private logginService: LoggingService) {
+
+    }
+
+    statusUpdated = new EventEmitter<string>();
+
+    addAccount(name: string, status: string) {
+        this.accounts.push({ name: name, status: status });
+        this.logginService.logStatusChange(status);
+    }
+
+    updateStatus(id: number, status: string) {
+        this.accounts[id].status = status;
+        this.logginService.logStatusChange(status);
+    }
+
+}
